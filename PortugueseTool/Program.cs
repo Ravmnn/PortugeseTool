9243f72d1df27e9cdcaf56231647a315d5e721d5
= <=>
2025-07-06 13:35:18 -0400

    add basic CLI application model infrastructure.

--- PortugueseTool/Program.cs
+++ PortugueseTool/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace PortugueseTool;
+﻿using System.CommandLine;
+
+
+namespace PortugueseTool;
 
 
 class Program
 {
-    static void Main(string[] args)
+    public static int Main(string[] args)
     {
-        Console.WriteLine("Hello, World in pt-br!");
+        var root = new RootCommand("CLI tool for analyzing portuguese text.");
+
+        var parseResult = root.Parse(args);
+
+        foreach (var parseError in parseResult.Errors)
+        {
+            Console.WriteLine(parseError.Message);
+            return 1;
+        }
+
+        return parseResult.Invoke();
     }
 }
